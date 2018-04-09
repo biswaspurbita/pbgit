908ef8b645d52eb77c173a8714a08711637f6c2d
biswaspurbita <38069666+biswaspurbita@users.noreply.github.com>
2018-04-09 19:33:12 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class helloworld 
+{
+ public static void main(String[] argv) 
+  {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello World");
+		System.out.println(argv.length);
+   }
+}
